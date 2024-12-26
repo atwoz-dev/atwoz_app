16adec7f6887b14d4ca0d927f363d8b4eb27ee97
GeumBinLee <lgb9811@gmail.com>
2024-12-26 17:30:28 +0900

    ✨ Feat: 공통 상태 및 페이지 관리 기능 추가
    - CommonState 정의 및 Freezed 기반 상태 전환 구현
    - CommonStateNotifier 추가: 공통 상태 관리 로직 구현
    - BasePageBaseState 구현: 공통 페이지 구조 및 로딩 상태 처리
    - AppCircularProgressIndicator 추가: iOS 스타일 로딩 인디케이터 구현

---
+++ lib/core/base/notifier/common_state.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'common_state.freezed.dart';
+
+/// 상태 정의
+@freezed
+class CommonState with _$CommonState {
+  const factory CommonState.initial() = _Initial; // 초기 상태
+  const factory CommonState.loading() = _Loading; // 로딩 상태
+  const factory CommonState.success() = _Success; // 성공 상테
+  const factory CommonState.error([Exception? exception]) = _Error; // 에러 상태
+
+  const CommonState._(); // Freezed에서 private constructor를 생성합니다.
+
+  // isLoading Getter 추가
+  bool get isLoading => this is _Loading;
+}
