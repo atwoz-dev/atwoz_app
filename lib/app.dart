8d446c1289fc4ce9062ff29525186cffb9996eae
GeumBinLee <lgb9811@gmail.com>
2024-12-26 20:59:31 +0900

    ✨ Feat: 애플리케이션 초기화 및 라우팅 구현
    - **앱 초기화 (`main.dart`)**:
      - `App.preserveSplash`를 통해 스플래시 화면 유지
      - 기기 방향을 세로로 고정 (`SystemChrome.setPreferredOrientations`)
      - `Hive` 데이터베이스 초기화 및 `User` 모델 어댑터 등록
      - `ProviderScope`로 전역 상태 관리 설정
    
    - **앱 컴포넌트 (`app.dart`)**:
      - `ScreenUtilInit`를 통한 반응형 UI 지원
      - `MaterialApp.router`를 사용하여 Riverpod 상태 기반 테마, 로케일, 라우팅 관리
      - 로딩 및 에러 상태에 따른 화면 렌더링 처리
    
    - **라우팅 (`routing.dart`)**:
      - `GoRouter`를 통해 라우트 관리
      - 인증 상태에 따른 리디렉션 로직 구현
      - 주요 경로:
        - `/` (홈 화면)
        - `/auth` (로그인 화면)
        - `/root` (설정 화면)
      - 에러 화면 처리 (`ErrorRoutePage`)

---
+++ lib/app.dart
@@ -0,0 +1,87 @@
+import 'dart:ui';
+import 'package:atwoz_app/core/base/notifier/app_notifier.dart';
+import 'package:atwoz_app/core/widgets/error/error_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:atwoz_app/router/routing.dart';
+
+import 'package:atwoz_app/config/config.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appNotifierAsync = ref.watch(appNotifierProvider);
+    final router = ref.watch(routerProvider);
+
+    return appNotifierAsync.when(
+      data: (appState) {
+        // Splash 화면 종료
+        removeSplash();
+
+        return ScreenUtilInit(
+          designSize: const Size(360, 800),
+          minTextAdapt: true,
+          child: MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            scrollBehavior:
+                const MaterialScrollBehavior().copyWith(dragDevices: {
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.touch,
+              PointerDeviceKind.trackpad,
+              PointerDeviceKind.stylus,
+              PointerDeviceKind.unknown,
+            }),
+            themeMode: ThemeMode.values[appState.themeModeIndex],
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            locale: Locale(appState.languageCode),
+            routerConfig: router,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+          ),
+        );
+      },
+      loading: () => const MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        ),
+      ),
+      error: (err, stack) => MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: Config.enableErrorPage
+
+            /// [AppErrorWidget] 존재
+            ? AppErrorPage(FlutterErrorDetails(
+                exception: err,
+                stack: stack,
+              ))
+            : Scaffold(
+                body: Center(
+                  child: Text('앱 로딩 실패: $err'),
+                ),
+              ),
+      ),
+    );
+  }
+
+  static WidgetsBinding? _widgetsBinding;
+
+  static void preserveSplash({required WidgetsBinding widgetsBinding}) {
+    _widgetsBinding = widgetsBinding;
+    _widgetsBinding?.deferFirstFrame();
+  }
+
+  static void removeSplash() {
+    if (_widgetsBinding == null) return;
+    Future.delayed(
+        const Duration(seconds: 1), _widgetsBinding?.allowFirstFrame);
+    _widgetsBinding = null;
+  }
+}
---
+++ lib/router/routing.dart
@@ -0,0 +1,72 @@
+import 'package:atwoz_app/core/widgets/error/error_route_page.dart';
+import 'package:atwoz_app/data/sources/remote/auth_service.dart';
+import 'package:atwoz_app/features/auth/presentation/sign_in_page.dart';
+import 'package:atwoz_app/features/home/presentation/home_page.dart';
+import 'package:atwoz_app/features/root/presentation/root_setting_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'routing.g.dart';
+
+final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+
+@riverpod
+class UserNotifier extends _$UserNotifier with ChangeNotifier {
+  bool get isAuthenticated => ref.watch(authServiceProvider).user != null;
+
+  @override
+  bool build() {
+    return isAuthenticated;
+  }
+
+  void login() {
+    // 로그인 후 상태를 변경
+    notifyListeners(); // 상태 변경 알림
+  }
+
+  void logout() {
+    // 로그아웃 후 상태를 변경
+    notifyListeners(); // 상태 변경 알림
+  }
+}
+
+@riverpod
+GoRouter router(Ref ref) {
+  final isAuthenticated = ref.watch(userNotifierProvider);
+
+  return GoRouter(
+    navigatorKey: rootNavigatorKey,
+    initialLocation: '/auth',
+    debugLogDiagnostics: true,
+    refreshListenable: ref.read(userNotifierProvider.notifier),
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/auth',
+        builder: (context, state) => SignInPage(),
+      ),
+      GoRoute(
+        path: '/root',
+        builder: (context, state) => const RootSettingPage(),
+      ),
+    ],
+    errorBuilder: (context, state) => ErrorRoutePage(state),
+    redirect: (context, state) {
+      final currentLocation = state.uri.toString();
+
+      // if (!isAuthenticated && currentLocation != '/auth') {
+      //   return '/auth';
+      // }
+      // if (isAuthenticated && currentLocation == '/auth') {
+      //   return '/';
+      // }
+      // return null; // 그대로 유지
+      return '/';
+    },
+  );
+}
