de4fc01a99f5afb246a1a0e7a98ca68fcbf523d4
GeumBinLee <lgb9811@gmail.com>
2024-12-26 20:45:29 +0900

    ✨ Feat: 네트워크 및 API 통신 관련 클래스 추가
    - `FileRepository`:
      - 텍스트 에디터 내용 저장 및 파일 업로드 기능 구현
      - FormData 변환 및 파일 처리 로직 포함
    - `ApiServiceImpl`:
      - API 요청 구현체로 `Dio`를 활용한 HTTP 요청 처리
      - 인증 토큰 추가 및 다양한 HTTP 메서드 지원 (`GET`, `POST`, 등)
    - `ApiService`:
      - 네트워크 인터페이스 정의
      - 사용자 정의 Base URL 및 인증 지원
    - `DioService`:
      - `Dio` 기반 네트워크 요청 처리 클래스
      - 요청 취소 기능 포함
    - `LoggingInterceptor`:
      - 요청/응답 데이터 로깅 인터셉터
      - cURL 명령어 생성을 통한 디버깅 지원
    - `NetworkException`:
      - 네트워크 예외 처리 클래스
      - 다양한 예외 유형 관리 (연결, 인증, 데이터 형식 등)
    - `TokenInterceptor`:
      - 인증 토큰 추가 및 갱신 처리
      - 401 및 403 에러 상황에 대한 처리 로직 포함

---
+++ lib/data/sources/remote/api_service.dart
@@ -0,0 +1,99 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'api_service_impl.dart';
+
+typedef Json = Map<String, dynamic>;
+typedef FromJson<T> = T Function(Json json);
+typedef ToJson<T> = Json Function(T data);
+typedef Converter<T> = T Function(dynamic data);
+typedef Progress = void Function(int count, int total);
+
+/// HTTP networking interface
+abstract class ApiService {
+  const ApiService();
+
+  /// `ref`를 필수로 받아야 하도록 수정
+  factory ApiService.create(Ref ref) =>
+      ApiServiceImpl(ref: ref, enableAuth: true);
+
+  /// 커스텀 baseUrl 설정을 위한 팩토리 메서드
+  factory ApiService.custom(Ref ref, {String? baseUrl}) =>
+      ApiServiceImpl(ref: ref, baseUrl: baseUrl);
+
+  void cancelRequests({CancelToken? cancelToken});
+
+  Future<T> request<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+    void Function(int, int)? onSendProgress,
+    void Function(int, int)? onReceiveProgress,
+    required String method,
+    required String contentType,
+    bool requiresAuthToken = true,
+    T Function(dynamic)? converter,
+    String? business,
+    Map<String, dynamic>? headers,
+  });
+
+  Future<T> deleteJson<T>(
+    String path, {
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+  });
+
+  Future<T> getJson<T>(
+    String path, {
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+    String? contentType,
+    String? business,
+    Map<String, String>? headers,
+  });
+
+  Future<T> putJson<T>(
+    String path, {
+    Object? data,
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+  });
+
+  Future<T> postJson<T>(
+    String path, {
+    required Object? data,
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+    String? business,
+    Map<String, dynamic>? headers,
+  });
+
+  Future<T> postFormUrlEncoded<T>(
+    String path, {
+    required Object? data,
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+  });
+
+  Future<T> putFormUrlEncoded<T>(
+    String path, {
+    Object? data,
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+  });
+
+  Future<T> postFormData<T>(
+    String path, {
+    required FormData data,
+    Json? queryParameters,
+    bool requiresAuthToken = true,
+    Converter<T>? converter,
+  });
+}
---
+++ lib/data/sources/remote/token_interceptor.dart
@@ -0,0 +1,78 @@
+import 'package:atwoz_app/data/sources/remote/auth_service.dart';
+import 'package:atwoz_app/router/routing.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Add token in header call API
+class TokenInterceptor extends Interceptor {
+  final Ref ref;
+
+  TokenInterceptor(this.ref) : super();
+
+  @override
+  Future<void> onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    if (options.headers.containsKey('requiresAuthToken')) {
+      if (options.headers['requiresAuthToken'] == true) {
+        final String? token =
+            await ref.read(authServiceProvider).getAuthToken();
+        options.headers.addAll(<String, Object?>{'Authorization': token});
+      }
+
+      options.headers.remove('requiresAuthToken');
+    }
+    return handler.next(options);
+  }
+
+  @override
+  Future<void> onError(
+      DioException err, ErrorInterceptorHandler handler) async {
+    final AuthService authService = ref.read(authServiceProvider);
+    final router = ref.read(routerProvider);
+
+    // 403 에러 발생 시 처리 로직
+    if (err.response?.statusCode == 403) {
+      router.go('/'); // 홈 경로로 이동
+    }
+
+    // 401 에러 발생 시 처리 로직
+    if (err.response?.statusCode == 401) {
+      final RequestOptions options = err.requestOptions;
+
+      // 리스폰스에서 에러 코드 또는 메시지 확인
+      final dynamic responseBody = err.response?.data;
+      final String? errorCode = responseBody['status'] as String?;
+
+      // 401 중에서도 토큰 만료 에러 코드(E1041)인 경우에만 토큰 재발급 시도
+      if (errorCode == 'E1041') {
+        try {
+          // 토큰 재발급
+          final Map<String, dynamic>? newToken =
+              await authService.refreshToken();
+
+          if (newToken != null) {
+            // 새로 받아 온 토큰으로 Authorization 헤더 갱신
+            options.headers['Authorization'] = newToken['accessToken'];
+            options.headers['Refresh-Token'] = newToken['refreshToken'];
+
+            // 새로운 토큰으로 원래 요청 재시도
+            final clonedRequest = await Dio().fetch(options);
+            return handler.resolve(clonedRequest);
+          }
+        } catch (refreshError) {
+          if (refreshError is DioException) {
+            // 토큰 재발급 실패 시 로그아웃
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              authService.signOut().then((_) {
+                router.go('/auth'); // 인증 페이지로 이동
+              });
+            });
+          }
+        }
+      }
+    }
+
+    super.onError(err, handler);
+  }
+}
