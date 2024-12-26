744578c78788d796776a57a1352f852e3d60c08c
GeumBinLee <lgb9811@gmail.com>
2024-12-26 12:34:17 +0900

    ✨ Feat: 로깅을 위한 Log 클래스와  LogMixin 추가
    변경 사항:
    - Log 클래스:
      - `d()`: 디버그 로그 출력
      - `e()`: 에러 로그 출력 및 스택 트레이스 제공
      - `prettyJson()`: JSON 데이터를 보기 좋게 포맷팅하여 출력
      - `log()`: 공통 로그 출력 메서드, 조건에 따라 로그 출력 제어
    
    - Config 설정 연동:
      - `Config.enableGeneralLog`: 로그 출력 여부 제어
      - `Config.isPrettyJson`: JSON 포맷팅 여부 제어
    - LogMixin:
      - `logD()` 메서드: 디버그 로그 출력
        - 호출된 클래스 이름(runtimeType)을 자동으로 로그에 포함
      - `logE()` 메서드: 에러 로그 출력
        - 에러 메시지, 에러 객체, 스택 트레이스를 출력
    
    주요 기능:
    - 개발 모드(kDebugMode)에서만 로그를 활성화하여 릴리스 빌드에서는 로그를 차단 (불필요한 로그 릴리스 환경에서 차단)
    - 공통 로깅 메서드를 사용해 로그 출력의 일관성 유지
    - JSON 데이터 포맷팅으로 디버깅 시 데이터 가독성 향상
    - LogMixin을 통해 여러 클래스에서 공통적으로 로그 출력

---
+++ lib/core/base/mixin/log_mixin.dart
@@ -0,0 +1,30 @@
+import '../../utils/log_utils.dart';
+
+/// Common using log data, log error in class
+// mixin: 여러 클래스에 공통 기능을 추가할 수 있는 Dart의 기능
+// mixin을 통해 클래스에 중복 없이 로직을 추가할 수 있다.
+// 상속과 다르게 여러 클래스에 동시에 적용할 수 있다.
+mixin LogMixin on Object {
+// on Object: Object를 상속하는 클래스에서만 LogMixin 믹스인을 사용 가능
+// 사실상 모든 클래스에서 사용 가능함
+
+  // 디버그 로그 출력 메서드 logE()
+  void logD(String message, {DateTime? time}) {
+    Log.d(message, name: runtimeType.toString(), time: time);
+  }
+
+  void logE(
+    Object? errorMessage, {
+    Object? errorObject,
+    StackTrace? stackTrace,
+    DateTime? time,
+  }) {
+    Log.e(
+      errorMessage,
+      name: runtimeType.toString(), // 호출된 클래스 이름(runtimeType)을 자동으로 로그에 포함
+      errorObject: errorObject,
+      stackTrace: stackTrace,
+      time: time,
+    );
+  }
+}
