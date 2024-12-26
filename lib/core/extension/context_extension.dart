ed432dae32cbd424bf389659666c7f28176979fb
GeumBinLee <lgb9811@gmail.com>
2024-12-26 14:11:58 +0900

    ✨ Feat: 확장 함수 및 유틸리티 추가
    - Context 및 Widget 관련 확장 함수 추가
      - `context_extension.dart`: `BuildContext`의 다국어 지원, 테마, 화면 정보, 반응형 유틸리티 등
      - `widget_extension.dart`: `Widget`을 `Sliver`로 변환하는 메서드 추가
    
    - DateTime 관련 확장 함수 추가
      - `date_time_extension.dart`: 날짜/시간 포맷 변환, 상대적 시간 표현(예: "몇 분 전")
      - `date_format_extension.dart`: DateFormat의 안전한 파싱 지원
    
    - List 및 Iterable 관련 확장 함수 추가
      - `list_extension.dart`: 요소 추가/제거 및 매핑, 구분자 적용 등
      - `iterable_extension.dart`: 요소 추가/제거 및 새 Iterable 생성
    
    - 숫자(num) 관련 확장 함수 추가
      - `num_extension.dart`: 통화/퍼센트 포맷팅, 산술 연산 메서드 추가
    
    - 텍스트 스타일 및 문자열 관련 확장 함수 추가
      - `text_style_extension.dart`: `TextStyle`을 이탤릭, 굵게 변경
      - `string_extension.dart`: 문자열 트리밍, 정렬, 전화번호 포맷팅
    
    - 주요 변경 사항
      - 다양한 확장 함수로 코드 재사용성 향상
      - 화면 크기, 문자열 포맷팅, 리스트/컬렉션 처리 간소화

---
+++ lib/core/extension/context_extension.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+/// BuildContext에 다양한 유틸리티 메서드를 추가합니다.
+extension ContextExtension on BuildContext {
+  /// 다국어 반환
+  AppLocalizations get l10n => AppLocalizations.of(this);
+
+  /// 앱 테마 및 색 반환
+  ThemeData get theme => Theme.of(this);
+
+  /// [AppThemeData.of(context).appColors]과 유사
+  ColorScheme get appColors => theme.colorScheme;
+
+  /// 사용자 정의 테마 반환
+  ThemeData get appTheme => Theme.of(this);
+
+  /// 다크 모드 테마가 활성화되어 있는지 확인
+  bool get isDarkTheme => theme.brightness == Brightness.dark;
+
+  /// 가장 가까운 MediaQuery 조상의 platformBrightness를 반환하거나, 부모 조상이(ancestor) 존재하지 않는 경우 [Brightness.light]를 반환
+  Brightness get platformBrightness => MediaQuery.platformBrightnessOf(this);
+
+  /// 주어진 컨텍스트를 둘러싼 이 클래스의 가장 가까운 인스턴스의 상태를 반환
+  ScaffoldMessengerState get messenger => ScaffoldMessenger.of(this);
+
+  /// mediaQuerySize: 화면의 전체 크기를 반환
+  Size get mediaQuerySize => MediaQuery.sizeOf(this);
+
+  /// 화면 너비 반환
+  double get screenWidth => mediaQuerySize.width;
+
+  /// 화면 높이 반환
+  double get screenHeight => mediaQuerySize.height;
+
+  /// mediaQueryViewPadding: 안전 영역 패딩(e.g. notch)을 반환
+  EdgeInsets get mediaQueryViewPadding => MediaQuery.viewPaddingOf(this);
+
+  /// mediaQueryViewInsets: 키보드가 나타났을 때 화면 패딩을 반환
+  EdgeInsets get mediaQueryViewInsets => MediaQuery.viewInsetsOf(this);
+
+  /// mediaQueryOrientation: 가로 또는 세로 방향을 반환
+  Orientation get mediaQueryOrientation => MediaQuery.orientationOf(this);
+
+  /// [Theme.of(context).colorScheme]과 유사
+  ColorScheme get colorScheme => theme.colorScheme;
+
+  /// [Theme.of(context).textStyle]과 유사
+  TextTheme get textStyle => theme.textTheme;
+
+  /// 화면의 shortestSide를 반환
+  double get mediaQueryShortestSide => mediaQuerySize.shortestSide;
+
+  /// 기기가 가로 모드(Landscape mode) 모드인지 확인
+  bool get isLandscape => mediaQueryOrientation == Orientation.landscape;
+
+  /// 기기가 세로 모드(portrait mode)인지 검사
+  bool get isPortrait => mediaQueryOrientation == Orientation.portrait;
+
+  /// 화면의 가로와 세로 중 더 짧은 쪽의 길이가 600p보다 작은 경우 true 반환
+  bool get isPhone => mediaQueryShortestSide < 600;
+
+  /// 화면의 가로와 세로 중 더 짧은 쪽의 길이가 600p 이상인 경우 true 반환
+  bool get isSmallTablet => mediaQueryShortestSide >= 600;
+
+  /// 화면의 가로와 세로 중 더 짧은 쪽의 길이가 720p 이상인 경우 true 반환
+  bool get isLargeTablet => mediaQueryShortestSide >= 720;
+
+  /// 기기가 태블릿인지 검사
+  bool get isTablet => isSmallTablet || isLargeTablet;
+
+  /// 화면 크기에 따라 특정 값을 반환하는 반응형 유틸리티
+  T responsiveValue<T>({
+    required T mobile,
+    T? tablet,
+    T? desktop,
+    T? watch,
+  }) {
+    final double deviceWidth = mediaQuerySize.shortestSide;
+
+    if (deviceWidth >= 1200 && desktop != null) {
+      return desktop;
+    } else if (deviceWidth >= 600 && tablet != null) {
+      return tablet;
+    } else if (deviceWidth < 300 && watch != null) {
+      return watch;
+    } else {
+      return mobile;
+    }
+  }
+}
+
+/// `Widget`을 Sliver 위젯으로 변환하는 확장 메서드
+extension WidgetExtension on Widget {
+  SliverToBoxAdapter get toSliverBox => SliverToBoxAdapter(child: this);
+
+  SliverPadding toSliverPadding(EdgeInsetsGeometry padding) => SliverPadding(
+        padding: padding,
+        sliver: toSliverBox,
+      );
+}
---
+++ lib/core/extension/extension.dart
@@ -0,0 +1,5 @@
+export 'context_extension.dart';
+export 'date_time_extension.dart';
+export 'list_extension.dart';
+export 'num_extension.dart';
+export 'text_extension.dart';
