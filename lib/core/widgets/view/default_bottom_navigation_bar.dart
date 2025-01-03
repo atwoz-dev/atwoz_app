5eef328d579fa27e9c4ca72269b3327f65f14cf5
GeumBinLee <lgb9811@gmail.com>
2025-01-03 12:11:29 +0900

    ✨ Feat: DefaultBottomNavigationBar에 네비게이션 로직 추가 #10
    - DefaultBottomNavigationBar
      - `onPressed`를 `_NavItem` 클래스로 추상화하여 구성
      - 네비게이션 로직 추가 (예: `AppRoute.interview` 이동 처리)
    
    - AppTextFormField
      - `contentPadding` 기본값을 동적으로 계산하도록 수정
      - 글자 수 카운터 스타일 및 위치 조정
      - ReportPage에서 해당 인자 삭제

--- lib/core/widgets/view/default_bottom_navigation_bar.dart
+++ lib/core/widgets/view/default_bottom_navigation_bar.dart
@@ -1,7 +1,9 @@
 import 'package:atwoz_app/core/theme/theme.dart';
 import 'package:atwoz_app/core/widgets/image/app_icon.dart';
+import 'package:atwoz_app/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 
 class DefaultBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -17,20 +19,50 @@ class DefaultBottomNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final appColors = context.appColors;
 
+    // BottomNavigationBarItem 정보를 간결하게 표현
     final items = [
-      {'icon': AppIcons.home, 'icon_fill': AppIcons.homeFill, 'label': '홈'},
-      {'icon': AppIcons.heart, 'icon_fill': AppIcons.heartFill, 'label': '좋아요'},
-      {'icon': AppIcons.bolt, 'icon_fill': AppIcons.boltFill, 'label': '셀프소개'},
-      {'icon': AppIcons.exam, 'icon_fill': AppIcons.examFill, 'label': '모의고사'},
-      {'icon': AppIcons.user, 'icon_fill': AppIcons.userFill, 'label': 'MY'},
+      _NavItem(
+        icon: AppIcons.home,
+        iconFill: AppIcons.homeFill,
+        label: '홈',
+        onPressed: () {},
+      ),
+      _NavItem(
+        icon: AppIcons.heart,
+        iconFill: AppIcons.heartFill,
+        label: '좋아요',
+        onPressed: () {},
+      ),
+      _NavItem(
+        icon: AppIcons.bolt,
+        iconFill: AppIcons.boltFill,
+        label: '셀프소개',
+        onPressed: () => navigate(
+          context,
+          route: AppRoute.interview,
+          method: NavigationMethod.go,
+        ),
+      ),
+      _NavItem(
+        icon: AppIcons.exam,
+        iconFill: AppIcons.examFill,
+        label: '모의고사',
+        onPressed: () {},
+      ),
+      _NavItem(
+        icon: AppIcons.user,
+        iconFill: AppIcons.userFill,
+        label: 'MY',
+        onPressed: () {},
+      ),
     ];
 
     final defaultTextStyle = AppStyles.body03Regular(AppColors.colorGrey400)
         .copyWith(fontSize: 10.sp);
 
     return Container(
-      padding: EdgeInsets.only(top: 20),
-      decoration: BoxDecoration(
+      padding: const EdgeInsets.only(top: 20),
+      decoration: const BoxDecoration(
         border: Border(
           top: BorderSide(
             color: AppColors.colorGrey100,
@@ -40,7 +72,10 @@ class DefaultBottomNavigationBar extends StatelessWidget {
       ),
       child: BottomNavigationBar(
         currentIndex: currentIndex,
-        onTap: onTap,
+        onTap: (index) {
+          items[index].onPressed?.call();
+          onTap(index);
+        },
         type: BottomNavigationBarType.fixed,
         selectedItemColor: appColors.primary,
         unselectedItemColor: AppColors.colorGrey800,
@@ -53,18 +88,32 @@ class DefaultBottomNavigationBar extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 AppIcon(
-                  isSelected ? item['icon_fill'] : item['icon'],
-                  colorFilter: isSelected
-                      ? AppIcon.fillColor(appColors.primary)
-                      : AppIcon.fillColor(AppColors.colorGrey400),
+                  isSelected ? item.iconFill : item.icon,
+                  colorFilter: AppIcon.fillColor(
+                      isSelected ? appColors.primary : AppColors.colorGrey400),
                 ),
-                SizedBox(height: 8.h), // 아이콘과 레이블 사이 간격 추가
+                Gap(8.h), // 아이콘과 레이블 사이 간격
               ],
             ),
-            label: item['label'],
+            label: item.label,
           );
         }).toList(),
       ),
     );
   }
+}
+
+// NavItem 클래스를 사용하여 아이템 정의 간소화
+class _NavItem {
+  final String icon;
+  final String iconFill;
+  final String label;
+  final VoidCallback? onPressed;
+
+  const _NavItem({
+    required this.icon,
+    required this.iconFill,
+    required this.label,
+    this.onPressed,
+  });
 }
