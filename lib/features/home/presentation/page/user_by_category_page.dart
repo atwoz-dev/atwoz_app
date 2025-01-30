89eb3bfb9072e3b4a8ee771895d1581ad26439f3
helljh <junho308917@naver.com>
2025-01-30 10:25:51 +0900

    ✨ Feat: 소개받고 싶은 이성 페이지 UI 완성

--- lib/features/home/presentation/page/user_by_category_page.dart
+++ lib/features/home/presentation/page/user_by_category_page.dart
@@ -1,11 +1,14 @@
 import 'dart:ui';
 
-import 'package:atwoz_app/app/constants/fonts.dart';
-import 'package:atwoz_app/app/constants/palette.dart';
+import 'package:atwoz_app/app/constants/constants.dart';
+import 'package:atwoz_app/app/router/router.dart';
+import 'package:atwoz_app/app/widget/button/default_elevated_button.dart';
+import 'package:atwoz_app/app/widget/icon/default_icon.dart';
 import 'package:atwoz_app/core/extension/extended_context.dart';
 import 'package:atwoz_app/features/home/presentation/widget/home_profile_card_area.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 class UserByCategoryPage extends StatefulWidget {
   const UserByCategoryPage({super.key});
@@ -23,9 +26,6 @@ class _UserByCategoryPageState extends State<UserByCategoryPage> {
     "취미가 같아요"
   ];
   int currentCategoryIndex = 0;
-  Map<int, bool> isBlurredMap = {
-    for (var item in List.generate(4, (index) => index)) item: true
-  };
   List<bool> isBlurredList =
       List<bool>.filled(10, true); // 10개의 아이템 모두 블러 처리 상태로 초기화
 
@@ -68,15 +68,27 @@ class _UserByCategoryPageState extends State<UserByCategoryPage> {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 18),
         child: ListView.separated(
-          itemCount: 4,
+          itemCount: isBlurredList.length,
           separatorBuilder: (context, index) => SizedBox(height: 8),
           itemBuilder: (context, index) {
             return UserByCategoryListItem(
-              isBlurred: isBlurredMap[index]!,
-              onTap: () {
-                setState(() {
-                  isBlurredMap[index] = !isBlurredMap[index]!; // 클릭 시 블러 상태 변경
-                });
+              isBlurred: isBlurredList[index],
+              onTap: () async {
+                // Make onTap async
+                if (isBlurredList[index]) {
+                  // Only show dialog if item is blurred
+                  final result = await showDialog<bool>(
+                    context: context,
+                    builder: (context) => ProfileOpenHearUseModal(),
+                  );
+
+                  if (result == true) {
+                    setState(() {
+                      isBlurredList[index] =
+                          false; // Update state based on dialog result
+                    });
+                  }
+                }
               },
             );
           },
@@ -101,83 +113,184 @@ class UserByCategoryListItem extends StatelessWidget {
     List<String> hashTagList = ["#클라이밍", "#불교", "#무계획 여행", "+2"];
     return GestureDetector(
       onTap: onTap,
-      child: BackdropFilter(
-        filter: isBlurred
-            ? ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0)
-            : ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0),
-        child: Container(
-          margin: EdgeInsets.only(top: 8),
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-              color: Palette.gray10, borderRadius: BorderRadius.circular(16)),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  SizedBox(
-                    height: 54,
-                    width: 54,
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage("assets/images/home_pic.png"),
-                      radius: 50,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(width: 16),
-              Expanded(
-                child: Column(
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 8),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: Palette.gray10, borderRadius: BorderRadius.circular(16)),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
                   children: [
-                    Container(
-                      // 해시태그 리스트 뷰
-                      width: context.screenHeight,
-                      height: 18,
-                      padding: EdgeInsets.symmetric(horizontal: 5),
-                      child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: hashTagList.length,
-                        itemBuilder: (context, index) {
-                          return HomeHashtagWidget(tagName: hashTagList[index]);
-                        },
-                        separatorBuilder: (context, index) {
-                          return SizedBox(width: 8);
-                        },
+                    SizedBox(
+                      height: 54,
+                      width: 54,
+                      child: CircleAvatar(
+                        backgroundImage:
+                            AssetImage("assets/images/home_pic.png"),
+                        radius: 50,
                       ),
                     ),
-                    SizedBox(height: 8),
-                    Text(
-                      "안녕하세요 활발한 성격의 유쾌하고 대화 코드가 맞는 자존감 높으신 분이 좋아요...",
-                      style: Fonts.body02Medium().copyWith(
-                          fontWeight: FontWeight.w400,
-                          color: Palette.colorGrey600),
-                    ),
-                    SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                          decoration: BoxDecoration(
-                              color: Palette.colorGrey100,
-                              borderRadius: BorderRadius.circular(12)),
-                          child: SvgPicture.asset(
-                            "assets/icons/home_heart.svg",
-                            width: 24,
-                            height: 24,
-                            color: Palette.colorPrimary500,
-                          ),
-                        )
-                      ],
-                    )
                   ],
                 ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Container(
+                        // 해시태그 리스트 뷰
+                        width: context.screenHeight,
+                        height: 18,
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: hashTagList.length,
+                          itemBuilder: (context, index) {
+                            return HomeHashtagWidget(
+                                tagName: hashTagList[index]);
+                          },
+                          separatorBuilder: (context, index) {
+                            return SizedBox(width: 8);
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "안녕하세요 활발한 성격의 유쾌하고 대화 코드가 맞는 자존감 높으신 분이 좋아요...",
+                        style: Fonts.body02Medium().copyWith(
+                            fontWeight: FontWeight.w400,
+                            color: Palette.colorGrey600),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 6),
+                              decoration: BoxDecoration(
+                                  color: Palette.colorGrey100,
+                                  borderRadius: BorderRadius.circular(12)),
+                              child: SvgPicture.asset(
+                                "assets/icons/home_heart.svg",
+                                width: 24,
+                                height: 24,
+                                color: Palette.colorPrimary500,
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (isBlurred)
+            Positioned.fill(
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
               ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileOpenHearUseModal extends StatelessWidget {
+  const ProfileOpenHearUseModal({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: context.screenWidth,
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          padding: EdgeInsets.fromLTRB(13, 24, 13, 16),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(12)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "소개 받으시겠습니까?",
+                style: Fonts.header02().copyWith(fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
+              Gap(12),
+              Text(
+                "보유한 하트:10",
+                style: Fonts.body02Medium().copyWith(
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff7E7E7E),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Gap(24),
+              Row(
+                children: [
+                  Expanded(
+                    child: DefaultElevatedButton(
+                      onPressed: () {
+                        pop(context);
+                      },
+                      primary: Colors.white,
+                      border: BorderSide(color: Color(0xffE1E1E1)),
+                      child: Text(
+                        "취소",
+                        style: Fonts.body02Medium().copyWith(
+                            fontWeight: FontWeight.w400,
+                            color: Palette.colorBlack),
+                      ),
+                    ),
+                  ),
+                  Gap(8),
+                  Expanded(
+                    child: DefaultElevatedButton(
+                      onPressed: () {
+                        pop(context, true);
+                      },
+                      primary: Palette.colorPrimary500,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          DefaultIcon(IconPath.homeHeart, size: 16),
+                          Gap(4),
+                          Text(
+                            "8",
+                            style: Fonts.body02Medium().copyWith(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
