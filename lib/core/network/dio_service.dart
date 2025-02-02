71f399892e0c989e2236a960b9f4eb838559a932
GeumBinLee <lgb9811@gmail.com>
2025-02-02 10:00:00 +0000

    ♻️ Refactor: DioService 쿠키 관리 및 로깅 기능 개선 #26
    - `PersistCookieJar`를 활용하여 쿠키 저장소를 `getApplicationDocumentsDirectory()` 경로에 저장하도록 변경
    - `_initializeCookieJar()` 메서드를 추가하여 쿠키 저장소를 비동기 초기화
    - `CookieManager`를 활용한 쿠키 자동 관리 기능 추가
    - `getStoredCookies()` 메서드를 추가하여 특정 `Uri`에 대한 저장된 쿠키 조회 가능하도록 구현
    - `LoggingInterceptor` 추가하여 API 요청 및 응답 로깅 기능 강화
    - 불필요한 `CookieJar()` 인스턴스 생성 제거 및 `_cookieJar`를 정적으로 관리하여 일관성 유지

--- lib/core/network/dio_service.dart
+++ lib/core/network/dio_service.dart
@@ -1,19 +1,30 @@
+import 'package:atwoz_app/core/network/logging_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
+import 'package:path_provider/path_provider.dart';
 
 /// Common service call API support cancel api
 class DioService extends DioForNative {
+  static late PersistCookieJar _cookieJar; // 쿠키 저장소 초기화
+
   DioService([super.baseOptions, Iterable<Interceptor>? interceptors])
       : _cancelToken = CancelToken() {
-    // 쿠키 관리를 포함한 인터셉터 추가
     interceptors ??= [];
+    _initializeCookieJar(); // 쿠키 저장소 초기화 함수 호출 (비동기)
+
     interceptors = [
       ...interceptors,
-      CookieManager(CookieJar()), // 쿠키 관리자 추가
+      CookieManager(_cookieJar), // 쿠키 관리자 추가
+      LoggingInterceptor(), // LoggingInterceptor 추가 (요청 로깅)
     ];
     this.interceptors.addAll(interceptors);
+  }
+
+  static Future<void> _initializeCookieJar() async {
+    final appDocDir = await getApplicationDocumentsDirectory();
+    _cookieJar = PersistCookieJar(storage: FileStorage(appDocDir.path));
   }
 
   final CancelToken _cancelToken;
@@ -24,6 +35,11 @@ class DioService extends DioForNative {
     } else {
       cancelToken.cancel(reason);
     }
+  }
+
+  /// 저장된 쿠키 가져오는 메서드
+  Future<List<Cookie>> getStoredCookies(Uri uri) async {
+    return _cookieJar.loadForRequest(uri);
   }
 
   @override
