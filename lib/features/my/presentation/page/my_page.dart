2a0641a887c15fd1352cccb4085088f3fe31eca3
helljh <97391158+helljh@users.noreply.github.com>
2025-03-07 17:42:26 +0900

    🩹Fix: global private const 변수로 변경

--- lib/features/my/presentation/page/my_page.dart
+++ lib/features/my/presentation/page/my_page.dart
@@ -4,29 +4,29 @@ import 'package:atwoz_app/app/widget/view/default_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+const List<String> _featureNames = [
+  '프로필 관리',
+  '이상형 설정',
+  '지인차단',
+  '스토어',
+  '고객센터',
+  '설정'
+];
+
+const List<String> _iconPaths = [
+  IconPath.myProfile,
+  IconPath.idealSetting,
+  IconPath.blockFriend,
+  IconPath.store,
+  IconPath.customerCenter,
+  IconPath.setting
+];
+
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<String> featureNames = [
-      '프로필 관리',
-      '이상형 설정',
-      '지인차단',
-      '스토어',
-      '고객센터',
-      '설정'
-    ];
-
-    final List<String> iconPaths = [
-      IconPath.myProfile,
-      IconPath.idealSetting,
-      IconPath.blockFriend,
-      IconPath.store,
-      IconPath.customerCenter,
-      IconPath.setting
-    ];
-
     return Scaffold(
       appBar: DefaultAppBar(
         title: "마이페이지",
@@ -35,11 +35,11 @@ class MyPage extends StatelessWidget {
           shrinkWrap: true,
           itemBuilder: (context, index) {
             return MyPageListItem(
-              title: featureNames[index],
-              iconPath: iconPaths[index],
+              title: _featureNames[index],
+              iconPath: _iconPaths[index],
             );
           },
-          itemCount: featureNames.length),
+          itemCount: _featureNames.length),
     );
   }
 }
