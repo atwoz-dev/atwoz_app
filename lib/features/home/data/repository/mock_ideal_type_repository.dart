fa5b22caaf3637efb48413c8e82fbcd4ad2db96b
helljh <junho308917@naver.com>
2025-05-31 15:53:16 +0900

    :recycle: Refactor: Map -> IdealTypeDto로 목데이터 변경

--- lib/features/home/data/repository/mock_ideal_type_repository.dart
+++ lib/features/home/data/repository/mock_ideal_type_repository.dart
@@ -6,24 +6,24 @@ final idealTypeRepository = Provider<MockIdealTypeRepository>((ref) {
 });
 
 class MockIdealTypeRepository {
-  Map<String, dynamic> sampleIdealType = {
+  IdealTypeDto _mockData = IdealTypeDto(
     // TODO: api 연동 시 제거
-    "minAge": 20,
-    "maxAge": 30,
-    "hobbies": ['등산/클라이밍', '맛집탐방', '쇼핑'],
-    "cities": ['서울', '대구'],
-    "religion": null,
-    "smokingStatus": null,
-    "drinkingStatus": null
-  };
+    minAge: 20,
+    maxAge: 30,
+    hobbies: ['등산/클라이밍', '맛집탐방', '쇼핑'],
+    cities: ['서울', '대구'],
+    religion: null,
+    smokingStatus: null,
+    drinkingStatus: null,
+  );
 
   Future<IdealTypeDto> getIdealType() async {
     // TODO: 테스트용 코드. api 연동 시 변경
-    return IdealTypeDto.fromJson(sampleIdealType);
+    return _mockData;
   }
 
   Future<void> setIdealType(IdealTypeDto idealTypeDto) async {
     // TODO: 테스트용 코드. api 연동 시 변경
-    sampleIdealType = idealTypeDto.toJson();
+    _mockData = idealTypeDto;
   }
 }
