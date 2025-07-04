905540e291e72bd58b9ab88ea26fe25af1970457
goodmeet <jinjerkim@gmail.com>
2025-07-04 09:53:31 +0900

    feat: 하트사용내역 repository 구현

---
+++ lib/features/store/data/repository/store_repository.dart
@@ -0,0 +1,48 @@
+import 'package:atwoz_app/core/network/base_repository.dart';
+import 'package:atwoz_app/core/util/log.dart';
+import 'package:atwoz_app/features/store/data/dto/heart_list_response.dart';
+import 'package:atwoz_app/features/store/domain/provider/heart_list_state.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/src/widgets/scroll_view.dart';
+import 'package:flutter/src/widgets/text.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final storeRepositoryProvider = Provider<StoreRepository>((ref) {
+  return StoreRepository(ref);
+});
+
+class StoreRepository extends BaseRepository {
+  StoreRepository(Ref ref) : super(ref, '/heart-transactions');
+
+  Future<void> requestStore(int lastId) async {
+    await apiService.postJson(
+      path,
+      data: {
+        'lastId': lastId,
+      },
+    );
+  }
+
+  Future<HeartListData> getHeartTransactionList([
+    int? lastId,
+  ]) async {
+    final res = await apiService.getJson(
+      path,
+      queryParameters: lastId != null ? {'lastId': lastId} : null,
+    );
+    return _parseHeartList(res);
+  }
+
+  HeartListData _parseHeartList(dynamic res) {
+    if (res is! Map<String, dynamic> || res['data'] is! Map<String, dynamic>) {
+      Log.e('data type is not Map $res');
+      throw Exception();
+    }
+
+    final response = HeartListResponse.fromJson(res['data']);
+    return HeartListData(
+      transactions: response.transactions.map((e) => e.toModel()).toList(),
+      hasMore: response.hasMore,
+    );
+  }
+}
