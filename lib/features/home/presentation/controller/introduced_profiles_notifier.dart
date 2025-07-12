4913991e41f696d4627451b1598bf893d2b8e4dd
helljh <junho308917@naver.com>
2025-07-12 09:16:18 +0900

    :sparkles: Feat: 소개프로필 업데이트 기능 추가

--- lib/features/home/presentation/controller/introduced_profiles_notifier.dart
+++ lib/features/home/presentation/controller/introduced_profiles_notifier.dart
@@ -5,6 +5,8 @@ import 'package:atwoz_app/features/home/data/dto/introduced_profile_dto.dart';
 import 'package:atwoz_app/features/home/data/repository/introduced_profile_repository.dart';
 import 'package:atwoz_app/features/home/domain/domain.dart';
 import 'package:atwoz_app/features/home/domain/use_case/fetch_introduced_profiles_use_case.dart';
+import 'package:atwoz_app/features/home/domain/use_case/update_introduced_profile_use_case.dart';
+import 'package:atwoz_app/features/profile/domain/common/model.dart';
 import 'package:hive/hive.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -23,6 +25,7 @@ class IntroducedProfilesNotifier extends _$IntroducedProfilesNotifier {
     return profiles;
   }
 
+  /// 프로필 소개받기
   Future<void> openProfile({
     required int memberId,
     required IntroducedCategory category,
@@ -43,10 +46,10 @@ class IntroducedProfilesNotifier extends _$IntroducedProfilesNotifier {
     state = AsyncData(updatedProfiles);
 
     // 캐시된 데이터 삭제
-    final box = await Hive.openBox(IntroducedProfileDto.boxName);
-    box.delete(category.name);
+    await _deleteIntroducedProfilesFromHive(category.name);
   }
 
+  /// 좋아요 설정
   Future<void> setFavoriteType(int memberId, FavoriteType type) async {
     if (!state.hasValue) return;
     try {
@@ -60,9 +63,38 @@ class IntroducedProfilesNotifier extends _$IntroducedProfilesNotifier {
             (e) => e.memberId == memberId ? e.copyWith(favoriteType: type) : e,
           )
           .toList();
+
       state = AsyncData(updatedProfiles);
+
+      // 캐시된 데이터 삭제
+      await _deleteIntroducedProfilesFromHive(category.name);
     } catch (e) {
       Log.e(e);
     }
+  }
+
+  /// 프로필 업데이트
+  Future<void> updateProfile({
+    required int index,
+    required UserProfile detailProfile,
+    required IntroducedCategory category,
+  }) async {
+    if (!state.hasValue) return;
+    await ref.read(updateIntroducedProfileUseCaseProvider).execute(
+          index: index,
+          detailProfile: detailProfile,
+          category: category,
+        );
+
+    final profiles = await ref
+        .read(fetchIntroducedProfilesUseCaseProvider)
+        .execute(category);
+    state = AsyncData(profiles);
+  }
+
+  /// 카테고리별 소개받은 프로필 캐시 삭제
+  Future<void> _deleteIntroducedProfilesFromHive(String category) async {
+    final box = await Hive.openBox(IntroducedProfileDto.boxName);
+    await box.delete(category);
   }
 }
