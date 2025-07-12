cdec69bc0d7ed038ad7f6a84bbb4fc3eca7402f1
helljh <junho308917@naver.com>
2025-07-12 11:16:59 +0900

    🩹 Fix: 텍스트 높이 수정

--- lib/features/home/presentation/widget/category/user_by_category_list_item.dart
+++ lib/features/home/presentation/widget/category/user_by_category_list_item.dart
@@ -64,7 +64,7 @@ class UserByCategoryListItem extends ConsumerWidget {
                       ),
                       const Gap(8),
                       SizedBox(
-                        height: context.screenHeight * 0.1,
+                        height: context.screenHeight * 0.05,
                         child: Text(
                           profile.interviewContent,
                           maxLines: 2,
