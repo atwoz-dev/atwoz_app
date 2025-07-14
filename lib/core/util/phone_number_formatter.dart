03066663ca436a81f97b7086b59a87e8cc4d6981
goodmeet <jinjerkim@gmail.com>
2025-07-14 21:14:51 +0900

    refactor: PhoneNumberFormatter 함수 리팩토링

--- lib/core/util/phone_number_formatter.dart
+++ lib/core/util/phone_number_formatter.dart
@@ -6,22 +6,15 @@ class PhoneNumberFormatter extends TextInputFormatter {
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
-    var digits = newValue.text.replaceAll(RegExp(r'\D'), '');
+    final digits = newValue.text.replaceAll(RegExp(r'\D'), '');
+    final limitLength = digits.length.clamp(0, 11);
 
-    String formatted = '';
-
-    if (digits.length <= 3) {
-      formatted = digits;
-    } else if (digits.length <= 7) {
-      formatted = '${digits.substring(0, 3)}-${digits.substring(3)}';
-    } else if (digits.length <= 11) {
-      formatted =
-          '${digits.substring(0, 3)}-${digits.substring(3, 7)}-${digits.substring(7)}';
-    } else {
-      formatted =
-          '${digits.substring(0, 3)}-${digits.substring(3, 7)}-${digits.substring(7, 11)}';
-    }
-
+    final formatted = switch (digits.length) {
+      <= 3 => digits,
+      <= 7 => '${digits.substring(0, 3)}-${digits.substring(3)}',
+      _ =>
+        '${digits.substring(0, 3)}-${digits.substring(3, 7)}-${digits.substring(7, limitLength)}',
+    };
     return TextEditingValue(
       text: formatted,
       selection: TextSelection.collapsed(
