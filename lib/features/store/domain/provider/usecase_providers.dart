6cf25e00568fe5fab4f762469d937894dc831daf
goodmeet <jinjerkim@gmail.com>
2025-08-15 09:30:03 +0900

    fix: ref.watch로 의존성 선언하여 Provider의 재구성 가능성 보장

--- lib/features/store/domain/provider/usecase_providers.dart
+++ lib/features/store/domain/provider/usecase_providers.dart
@@ -3,5 +3,5 @@ import 'package:atwoz_app/features/store/domain/usecase/verify_receipt_usecase.d
 import 'package:atwoz_app/features/store/data/repository/store_repository.dart';
 
 final verifyReceiptUseCaseProvider = Provider<VerifyReceiptUseCase>(
-  (ref) => VerifyReceiptUseCase(ref.read(storeRepositoryProvider)),
+  (ref) => VerifyReceiptUseCase(ref.watch(storeRepositoryProvider)),
 );
