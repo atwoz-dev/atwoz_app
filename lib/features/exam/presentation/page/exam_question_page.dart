a4b5517a0b5ae1830194587451699668a46ed439
goodmeet <jinjerkim@gmail.com>
2025-08-29 14:25:34 +0900

    fix: 연애모의고사 테스트 화면 추가

---
+++ lib/features/exam/presentation/page/exam_question_page.dart
@@ -0,0 +1,314 @@
+import 'package:atwoz_app/app/router/router.dart';
+import 'package:atwoz_app/app/widget/widget.dart';
+import 'package:atwoz_app/features/exam/data/data.dart';
+import 'package:atwoz_app/features/exam/domain/provider/domain.dart';
+import 'package:atwoz_app/features/exam/presentation/widget/answer_radio_button.dart';
+import 'package:atwoz_app/features/exam/presentation/widget/step_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:atwoz_app/app/constants/palette.dart';
+import 'package:atwoz_app/app/constants/fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:atwoz_app/core/state/base_page_state.dart';
+import 'package:gap/gap.dart';
+
+class ExamQuestionPage extends ConsumerStatefulWidget {
+  const ExamQuestionPage({super.key});
+
+  @override
+  ExamQuestionPageState createState() => ExamQuestionPageState();
+}
+
+class ExamQuestionPageState
+    extends BaseConsumerStatefulPageState<ExamQuestionPage> {
+  ExamQuestionPageState()
+      : super(
+          isAppBar: false,
+          isHorizontalMargin: false,
+        );
+
+  PageController _pageController = PageController();
+
+  Map<int, int> _currentAnswerList = {};
+  Map<int, int> _requiredAnswerList = {};
+  Map<int, int> _optionalAnswerList = {};
+
+  int _currentPage = 0;
+
+  int get _currentSubjectIndex =>
+      ref.watch(examNotifierProvider.select((s) => s.currentSubjectIndex));
+
+  List<SubjectItem> get _subjectList =>
+      ref.watch(examNotifierProvider).questionList.questionList;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController.addListener(() {
+      final page = _pageController.page?.round() ?? 0;
+      if (page != _currentPage) {
+        setState(() {
+          _currentPage = page;
+        });
+      }
+    });
+  }
+
+  Map<String, dynamic> buildFinalPayload({
+    required bool isRequired,
+  }) {
+    final Map<int, int> answerList =
+        isRequired ? _requiredAnswerList : _optionalAnswerList;
+
+    final List<Map<String, dynamic>> subjects = [];
+
+    for (final subject in _subjectList) {
+      final List<Map<String, dynamic>> answers = [];
+
+      for (final question in subject.questions) {
+        final questionId = question.id;
+        final answerId = answerList[questionId];
+
+        if (answerId != null) {
+          answers.add({
+            "questionId": questionId,
+            "answerId": answerId,
+          });
+        }
+      }
+
+      if (answers.isNotEmpty) {
+        subjects.add({
+          "subjectId": subject.id,
+          "answers": answers,
+        });
+      }
+    }
+
+    return {
+      "subjects": subjects,
+    };
+  }
+
+  void _resetAnswer() {
+    setState(() {
+      _currentAnswerList = {};
+    });
+  }
+
+  void _nextStep() async {
+    final currentSubject = _subjectList[_currentSubjectIndex];
+
+    if (_currentPage < currentSubject.questions.length - 1) {
+      await Future.delayed(Duration(milliseconds: 300));
+
+      _pageController.nextPage(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _prevStep() {
+    if (_currentPage > 0) {
+      _pageController.previousPage(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _showLeaveExamDialogue();
+    }
+  }
+
+  void _showLeaveExamDialogue() {
+    CustomDialogue.showTwoChoiceDialogue(
+        context: context,
+        content: '연애 모의고사를 종료 하시겠어요?\n페이지를 벗어날경우, 저장되지 않아요',
+        onElevatedButtonPressed: () {
+          ref.read(examNotifierProvider.notifier).setOptional(true);
+          ref.read(examNotifierProvider.notifier).resetCurrentSubjectIndex();
+
+          navigate(
+            context,
+            route: AppRoute.home,
+          );
+        });
+  }
+
+  void _nextSubject() async {
+    _resetAnswer();
+
+    ref
+        .read(examNotifierProvider.notifier)
+        .setCurrentSubjectIndex(_currentSubjectIndex + 1);
+
+    setState(() {
+      _currentPage = 0;
+      _pageController.jumpToPage(0);
+    });
+  }
+
+  void _submit() {
+    final payloadMap = buildFinalPayload(isRequired: true);
+    final payload = ExamAnswerRequest.fromJson(payloadMap);
+    print(payload);
+    //ref.read(examNotifierProvider.notifier).createRequiredAnswerList(payload);
+
+    navigate(
+      context,
+      route: AppRoute.examResult,
+    );
+
+    if (_currentSubjectIndex < _subjectList.length - 1) {
+      _nextSubject();
+    } else {
+      ref.read(examNotifierProvider.notifier).setOptional(false);
+    }
+  }
+
+  @override
+  Widget buildPage(BuildContext context) {
+    final examState = ref.watch(examNotifierProvider);
+    final double horizontalPadding = screenWidth * 0.05;
+    final EdgeInsets contentPadding =
+        EdgeInsets.symmetric(horizontal: horizontalPadding);
+
+    if (!examState.isLoaded || _subjectList.isEmpty) {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    final currentSubject = _subjectList[_currentSubjectIndex];
+    final currentQuestions = currentSubject.questions;
+
+    return Scaffold(
+      appBar: DefaultAppBar(
+        title: currentSubject.name,
+        leadingAction: (context) => _showLeaveExamDialogue(),
+      ),
+      body: Padding(
+        padding: contentPadding,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Expanded(
+            flex: 9,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Gap(24),
+              StepIndicator(
+                totalStep: currentQuestions.length,
+                currentStep: _currentPage + 1,
+              ),
+              Gap(24),
+              ConstrainedBox(
+                constraints: BoxConstraints(minHeight: 60.h),
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${_currentPage + 1}. ",
+                        style: Fonts.header03().copyWith(
+                          color: Palette.colorBlack,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          currentQuestions[_currentPage].content,
+                          style: Fonts.header03().copyWith(
+                            color: Palette.colorBlack,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+              Expanded(
+                child: PageView.builder(
+                  controller: _pageController,
+                  physics: NeverScrollableScrollPhysics(), // 자동 Swipe 방지
+                  itemCount: currentQuestions.length,
+                  itemBuilder: (context, index) {
+                    final question = currentQuestions[index];
+                    final selectedAnswerId = _currentAnswerList[question.id];
+
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: question.answers.map((answer) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 12),
+                              child: AnswerRadioButton(
+                                id: answer.id,
+                                selectedId: selectedAnswerId,
+                                content: answer.content,
+                                onTap: (int id) {
+                                  setState(() {
+                                    _currentAnswerList[question.id] = id;
+                                    if (examState.isRequired) {
+                                      _requiredAnswerList[question.id] = id;
+                                    }
+
+                                    if (!examState.isRequired) {
+                                      _optionalAnswerList[question.id] = id;
+                                    }
+                                  });
+
+                                  _nextStep();
+                                },
+                              ),
+                            );
+                          }).toList(),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: screenHeight * 0.05),
+            child: Row(spacing: 8.0, children: [
+              Expanded(
+                  child: DefaultOutlinedButton(
+                primary: Palette.colorGrey100,
+                onPressed: () => _prevStep(),
+                child: Text('이전'),
+                textColor: Palette.colorGrey500,
+              )),
+              Expanded(
+                child: examState.isRequired
+                    ? DefaultElevatedButton(
+                        onPressed: _currentAnswerList.length !=
+                                currentSubject.questions.length
+                            ? null
+                            : () => _submit(),
+                        child: Text('제출하기'),
+                      )
+                    : DefaultElevatedButton(
+                        onPressed: _currentAnswerList.length !=
+                                currentSubject.questions.length
+                            ? null
+                            : () => {
+                                  _nextSubject(),
+                                  ref
+                                      .read(examNotifierProvider.notifier)
+                                      .setOptional(true),
+                                  ref
+                                      .read(examNotifierProvider.notifier)
+                                      .setExamDone(),
+                                },
+                        child: Text('다음'),
+                      ),
+              )
+            ]),
+          ),
+        ]),
+      ),
+    );
+  }
+}
