8f86096d15e8bf70161ca4fd04f629c164975a32
helljh <junho308917@naver.com>
2025-09-09 21:41:52 +0900

    Feat: SendReportUseCase 작성

---
+++ lib/features/report/domain/use_case/send_report_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:atwoz_app/features/report/data/mapper/report_mapper.dart';
+import 'package:atwoz_app/features/report/data/repository/report_repository.dart';
+import 'package:atwoz_app/features/report/domain/model/report.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final sendReportUseCaseProvider = Provider<SendReportUseCase>((ref) {
+  return SendReportUseCase(ref);
+});
+
+class SendReportUseCase {
+  final Ref _ref;
+
+  SendReportUseCase(this._ref);
+
+  Future<void> extecute(Report report) async {
+    final repository = _ref.read(reportRepositoryProvider);
+    await repository.sendReport(report.toDto());
+  }
+}
