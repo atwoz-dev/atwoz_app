af5566e2b04757a00825d5765c0f29449e8eb913
helljh <junho308917@naver.com>
2025-09-23 10:51:28 +0900

    Feat: 로그아웃 기능 구현

--- lib/app/provider/global_notifier.dart
+++ lib/app/provider/global_notifier.dart
@@ -2,9 +2,12 @@ import 'package:atwoz_app/app/state/global_state.dart';
 import 'package:atwoz_app/features/auth/data/usecase/auth_usecase_impl.dart';
 import 'package:atwoz_app/core/notification/firebase_manager.dart';
 import 'package:atwoz_app/core/util/util.dart';
+import 'package:atwoz_app/features/home/data/dto/introduced_profile_dto.dart';
 import 'package:atwoz_app/features/home/domain/model/cached_user_profile.dart';
 import 'package:atwoz_app/features/home/domain/use_case/get_profile_from_hive_use_case.dart';
 import 'package:atwoz_app/features/home/domain/use_case/save_profile_to_hive_use_case.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'global_notifier.g.dart';
@@ -57,5 +60,32 @@ class GlobalNotifier extends _$GlobalNotifier {
   Future<CachedUserProfile> fetchProfileToHiveFromServer() async {
     await ref.read(saveProfileToHiveUseCaseProvider).execute();
     return await getProfileFromHive();
+  }
+
+  Future<void> clearProfile() async {
+    try {
+      // CachedUserProfile 박스 열기 또는 참조
+      final profileBox = Hive.isBoxOpen(CachedUserProfile.boxName)
+          ? Hive.box<CachedUserProfile>(CachedUserProfile.boxName)
+          : await Hive.openBox<CachedUserProfile>(CachedUserProfile.boxName);
+
+      // IntroducedProfileDto 박스 열기 또는 참조
+      final introducedProfilesBox = Hive.isBoxOpen(IntroducedProfileDto.boxName)
+          ? Hive.box<IntroducedProfileDto>(IntroducedProfileDto.boxName)
+          : await Hive.openBox<IntroducedProfileDto>(
+              IntroducedProfileDto.boxName);
+
+      await profileBox.clear();
+      await profileBox.close();
+
+      state = state.copyWith(profile: CachedUserProfile.init());
+
+      await introducedProfilesBox.clear();
+      await introducedProfilesBox.close();
+
+      await const FlutterSecureStorage().deleteAll();
+    } catch (e) {
+      Log.e('Failed to clear profile: $e');
+    }
   }
 }
