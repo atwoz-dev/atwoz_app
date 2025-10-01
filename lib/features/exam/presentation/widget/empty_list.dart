da0d7656adc8476da6d5063f9713255e27cc4d22
goodmeet <jinjerkim@gmail.com>
2025-10-01 14:28:15 +0900

    feat: 소울메이트 빈리스트 위젯 추가

---
+++ lib/features/exam/presentation/widget/empty_list.dart
@@ -0,0 +1,32 @@
+import 'package:atwoz_app/app/router/router.dart';
+import 'package:atwoz_app/app/widget/button/button.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../../../../app/constants/icon_path.dart';
+import '../../../../app/widget/icon/default_icon.dart';
+
+class EmptyList extends StatelessWidget {
+  const EmptyList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const DefaultIcon(
+          IconPath.sadEmotion,
+          size: 48.0,
+        ),
+        const Gap(8.0),
+        Text(
+          '이번에는 소울메이트를 찾지 못했지만\n참여자가 늘어나서 같은 답안을\n선택한 이성이 나온다면 알려드릴께요',
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
