0bac12052e7dc9b25359adb68f3d0dc6545fcc73
goodmeet <jinjerkim@gmail.com>
2025-10-12 14:08:10 +0900

    fix: usecase 에러처리 통일

--- lib/features/exam/domain/usecase/exam_create_submit_usecase.dart
+++ lib/features/exam/domain/usecase/exam_create_submit_usecase.dart
@@ -1,7 +1,6 @@
 import 'package:atwoz_app/features/exam/domain/model/subject_answer.dart';
 import 'package:atwoz_app/features/exam/data/repository/exam_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:atwoz_app/core/util/log.dart';
 
 class ExamCreateSubmitUsecase {
   final Ref ref;
@@ -11,13 +10,8 @@ class ExamCreateSubmitUsecase {
   Future<void> call({
     required SubjectAnswer request,
   }) async {
-    try {
-      await ref.read(examRepositoryProvider).submitAnswerList(
-            request: request,
-          );
-    } catch (e) {
-      Log.e(e);
-      rethrow;
-    }
+    await ref.read(examRepositoryProvider).submitAnswerList(
+          request: request,
+        );
   }
 }
