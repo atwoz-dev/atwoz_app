747fb975873638c5512b88f97902671072efb143
asci <coffee5084@gmail.com>
2025-10-27 21:33:24 +0900

    Refactor: 메인 탭 관리 로직 개선
    - `MainTabType` enum을 추가하여 탭 정보를 관리하도록 변경.
    - `_tabs` 상수를 `main_tab_type.dart` 파일로 이동하고 `mainTabs`로 이름을 변경.
    - `BuildContext` extension을 추가하여 `MainTabType`을 통해 특정 탭으로 이동하는 `moveToMainTabOf` 메서드를 구현.
    - '보낸 좋아요' 목록이 비어있을 때, 홈 탭으로 이동하는 로직에 `moveToMainTabOf`를 적용.

--- lib/features/home/presentation/page/main_tab_view.dart
+++ lib/features/home/presentation/page/main_tab_view.dart
@@ -1,11 +1,7 @@
-import 'package:atwoz_app/app/constants/icon_path.dart';
+import 'dart:developer';
+
 import 'package:atwoz_app/app/widget/view/default_bottom_navigation_bar.dart';
-import 'package:atwoz_app/core/extension/extended_context.dart';
-import 'package:atwoz_app/features/favorite_list/presentation/page/favorite_list_page.dart';
-import 'package:atwoz_app/features/home/home.dart';
-import 'package:atwoz_app/features/interview/interview.dart';
-import 'package:atwoz_app/features/introduce/introduce.dart';
-import 'package:atwoz_app/features/my/my.dart';
+import 'package:atwoz_app/features/home/domain/model/main_tab_type.dart';
 import 'package:flutter/material.dart';
 
 class MainTabView extends StatefulWidget {
@@ -24,7 +20,7 @@ class _MainTabViewState extends State<MainTabView>
   void initState() {
     super.initState();
     _controller = TabController(
-      length: _tabs.length,
+      length: mainTabs.length,
       vsync: this,
       initialIndex: _currentIndex,
     )..addListener(() => setState(() => _currentIndex = _controller.index));
@@ -42,45 +38,22 @@ class _MainTabViewState extends State<MainTabView>
       extendBodyBehindAppBar: false,
       body: TabBarView(
         controller: _controller,
-        children: _tabs.map((item) => item.container).toList(),
+        children: mainTabs.map((item) => item.container).toList(),
       ),
       bottomNavigationBar: DefaultBottomNavigationBar(
         controller: _controller,
-        tabs: _tabs,
+        tabs: mainTabs,
       ),
     );
   }
 }
 
-const _tabs = [
-  TabItemInfo(
-    icon: IconPath.home,
-    iconFill: IconPath.homeFill,
-    label: '홈',
-    container: HomePage(),
-  ),
-  TabItemInfo(
-    icon: IconPath.like,
-    iconFill: IconPath.likeFill,
-    label: '좋아요',
-    container: FavoriteListPage(),
-  ),
-  TabItemInfo(
-    icon: IconPath.self,
-    iconFill: IconPath.selfFill,
-    label: '셀프소개',
-    container: IntroducePage(),
-  ),
-  TabItemInfo(
-    icon: IconPath.interview,
-    iconFill: IconPath.interviewFill,
-    label: '인터뷰',
-    container: InterviewPage(),
-  ),
-  TabItemInfo(
-    icon: IconPath.mypage,
-    iconFill: IconPath.mypageFill,
-    label: 'MY',
-    container: MyPage(),
-  ),
-];
+extension MainTabDefaultTabController on BuildContext {
+  void moveToMainTabOf(MainTabType tabType) {
+    final mainTabState = findAncestorStateOfType<_MainTabViewState>();
+    final controller = mainTabState?._controller;
+    final index = mainTabs.indexWhere((tab) => tab.label == tabType.label);
+    assert(controller != null, 'MainTab state is null');
+    controller?.animateTo(index);
+  }
+}
