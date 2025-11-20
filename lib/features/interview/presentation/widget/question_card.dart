925e3f61eb8be77a37e99733be92091bd99a9fe4
goodmeet <jinjerkim@gmail.com>
2025-11-20 10:50:38 +0900

    feat: 인터뷰삭제 ui 추가

--- lib/features/interview/presentation/widget/question_card.dart
+++ lib/features/interview/presentation/widget/question_card.dart
@@ -1,12 +1,12 @@
 import 'package:atwoz_app/app/router/route_arguments.dart';
 import 'package:atwoz_app/app/router/router.dart';
+import 'package:atwoz_app/app/widget/dialogue/dialogue.dart';
 import 'package:atwoz_app/core/state/base_widget_state.dart';
-import 'package:atwoz_app/features/interview/data/data.dart';
+import 'package:atwoz_app/core/util/toast.dart';
 import 'package:atwoz_app/features/interview/interview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
-import 'package:atwoz_app/features/interview/presentation/widget/interview_answer_tag_widget.dart';
 import 'package:atwoz_app/app/constants/constants.dart';
 
 class QuestionCard extends ConsumerStatefulWidget {
@@ -75,16 +75,42 @@ class _QuestionCardState extends AppBaseConsumerWidgetState<QuestionCard> {
                   context,
                   route: AppRoute.interviewRegister,
                   extra: InterviewRegisterArguments(
-                      question: question.questionContent,
-                      answer: question.answerContent ?? '',
-                      currentTabIndex: widget.currentTabIndex,
-                      answerId: question.answerId,
-                      questionId: question.questionId,
-                      isAnswered: question.isAnswered),
+                    question: question.questionContent,
+                    answer: question.answerContent ?? '',
+                    currentTabIndex: widget.currentTabIndex,
+                    answerId: question.answerId,
+                    questionId: question.questionId,
+                    isAnswered: question.isAnswered,
+                  ),
+                );
+              },
+              onLongPress: () {
+                if (!question.isAnswered) return;
+
+                CustomDialogue.showTwoChoiceDialogue(
+                  context: context,
+                  content: '작성하신 ${question.questionContent} 소개글을 삭제하시겠어요?',
+                  onElevatedButtonPressed: () async {
+                    final isSuccess = await ref
+                        .read(interviewProvider.notifier)
+                        .removeAnswer(question.answerId!, question.questionId);
+
+                    Navigator.of(context).pop();
+
+                    if (isSuccess) {
+                      showToastMessage('소개글이 정상적으로 삭제되었어요.');
+                      return;
+                    }
+
+                    showToastMessage('소개글을 삭제하는데 실패했습니다.');
+                  },
                 );
               },
               child: _buildQuestionCard(
-                  context, question.questionContent, question.isAnswered),
+                context,
+                question.questionContent,
+                question.isAnswered,
+              ),
             );
           }).toList(),
         ),
@@ -93,36 +119,36 @@ class _QuestionCardState extends AppBaseConsumerWidgetState<QuestionCard> {
   }
 
   Widget _buildQuestionCard(
-      BuildContext context, String question, bool isAnswered) {
+    BuildContext context,
+    String question,
+    bool isAnswered,
+  ) {
     return SizedBox(
-        width:
-            (screenWidth - widget.horizontalPadding * 2 - widget.tagSpacing) /
-                2,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Palette.colorGrey100.withAlpha(100),
-            borderRadius: Dimens.buttonRadius,
+      width:
+          (screenWidth - widget.horizontalPadding * 2 - widget.tagSpacing) / 2,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Palette.colorGrey100.withAlpha(100),
+          borderRadius: Dimens.buttonRadius,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: 60),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InterviewAnswerTagWidget(isAnswered),
+              const Gap(8),
+              Text(
+                question,
+                style: Fonts.body02Regular(palette.onSurface),
+                maxLines: 2,
+                overflow: TextOverflow.clip,
+              ),
+            ],
           ),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 15,
-          ),
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(minHeight: 60),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InterviewAnswerTagWidget(isAnswered),
-                const Gap(8),
-                Text(
-                  question,
-                  style: Fonts.body02Regular(palette.onSurface),
-                  maxLines: 2,
-                  overflow: TextOverflow.clip,
-                ),
-              ],
-            ),
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
