85fd8f0951d7201d06e8cbf1bd534927d86a2521
helljh <junho308917@naver.com>
2025-11-21 23:52:13 +0900

    Feat: 신고/차단 시 소개 프로필 Hive 박스 clear

--- lib/features/report/presentation/provider/report_notifier.dart
+++ lib/features/report/presentation/provider/report_notifier.dart
@@ -1,7 +1,9 @@
+import 'package:atwoz_app/features/home/data/dto/introduced_profile_dto.dart';
 import 'package:atwoz_app/features/report/domain/enum/report_reason.dart';
 import 'package:atwoz_app/features/report/domain/model/report.dart';
 import 'package:atwoz_app/features/report/domain/use_case/block_user_use_case.dart';
 import 'package:atwoz_app/features/report/domain/use_case/send_report_use_case.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'report_notifier.g.dart';
@@ -14,21 +16,23 @@ class ReportNotifier extends _$ReportNotifier {
   }
 
   set reason(String reason) {
-    state = state.copyWith(
-      reason: ReportReason.fromLabel(reason),
-    );
+    state = state.copyWith(reason: ReportReason.fromLabel(reason));
   }
 
   set content(String content) {
-    state = state.copyWith(
-      content: content,
-    );
+    state = state.copyWith(content: content);
   }
 
   Future<bool> report() async {
     try {
-      await ref.read(sendReportUseCaseProvider).execute(state);
-      return true;
+      final isReportSuccess = await ref
+          .read(sendReportUseCaseProvider)
+          .execute(state);
+
+      if (isReportSuccess) {
+        await _deleteCachedIntroducedProfiles();
+      }
+      return isReportSuccess;
     } catch (e) {
       return false;
     }
@@ -36,10 +40,22 @@ class ReportNotifier extends _$ReportNotifier {
 
   Future<bool> block() async {
     try {
-      await ref.read(blockUserUseCaseProvider).execute(state.reporteeId);
-      return true;
+      final isBlockSuccess = await ref
+          .read(blockUserUseCaseProvider)
+          .execute(state.reporteeId);
+
+      if (isBlockSuccess) {
+        await _deleteCachedIntroducedProfiles();
+      }
+
+      return isBlockSuccess;
     } catch (e) {
       return false;
     }
+  }
+
+  Future<void> _deleteCachedIntroducedProfiles() async {
+    final box = await Hive.openBox<Map>(IntroducedProfileDto.boxName);
+    await box.clear();
   }
 }
