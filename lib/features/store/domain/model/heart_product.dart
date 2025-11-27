7bf92cf0b8e2e77f5b8dd6ccc4442b0b6c03dfc6
goodmeet <jinjerkim@gmail.com>
2025-11-27 09:55:18 +0900

    fix: 스토어 가격 변경

--- lib/features/store/domain/model/heart_product.dart
+++ lib/features/store/domain/model/heart_product.dart
@@ -1,8 +1,8 @@
 enum HeartProduct {
-  heart45('APP_ITEM_HEART_45', 45, 9000),
+  heart45('APP_ITEM_HEART_45', 45, 9900),
   heart90('APP_ITEM_HEART_90', 90, 15000),
   heart110('APP_ITEM_HEART_110', 110, 22000),
-  heart350('APP_ITEM_HEART_350', 350, 59000),
+  heart350('APP_ITEM_HEART_350', 350, 55000),
   heart550('APP_ITEM_HEART_550', 550, 88000);
 
   final String code;
